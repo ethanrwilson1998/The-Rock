4a84c3b1e298e304a7889173f961c5628e79d89d
ethanrwilson1998 <43278339+ethanrwilson1998@users.noreply.github.com>
2019-04-08 14:00:35 -0400

    parallax title screen

---
+++ The Rock/Assets/Scripts/SimpleOscillate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleOscillate : MonoBehaviour
+{
+    [SerializeField] private bool useSin;
+    [SerializeField] private Vector3 posOffset;
+    [SerializeField] private float timeScale;
+
+    private Vector3 start;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        start = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (useSin)
+        {
+            transform.position = start + posOffset * Mathf.Sin(Time.time * timeScale);
+        }
+        else
+        {
+            transform.position = start + posOffset * Mathf.Cos(Time.time * timeScale);
+        }
+    }
+}
