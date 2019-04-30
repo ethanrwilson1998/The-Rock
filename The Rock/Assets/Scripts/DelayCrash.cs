ab8ff1efe03fde129862c36b33e8bb298ca33198
Kevin Tang <kev123361@gmail.com>
2019-04-30 13:07:10 -0400

    Rock falling sfx

---
+++ The Rock/Assets/Scripts/DelayCrash.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayCrash : MonoBehaviour
+{
+    private AudioSource source;
+
+    public float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        StartCoroutine(Delaying());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator Delaying()
+    {
+        yield return new WaitForSecondsRealtime(time);
+        source.Play();
+    }
+}
